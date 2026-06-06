51f39ee5839d7f7a90940318a32707ed237d5016
Hamza Plojovic <hamzaplojovic9@gmail.com>
2026-06-06 22:14:33 +0200

    chore: bump swarm to 0.2.0

--- Casks/swarm.rb
+++ Casks/swarm.rb
@@ -1,8 +1,8 @@
 cask "swarm" do
   arch arm: "aarch64"
 
-  version "0.1.0"
-  sha256 "12fedcd4947de8bf383cd2d38354476b395883ffb53052ad37e48df54ef4cd47"
+  version "0.2.0"
+  sha256 "c1fac826ca6f6031f67fb201d711abea6307ee7b57bf815ce7cc8e8a7b819f4a"
 
   url "https://github.com/waterloo-org/swarm-releases/releases/download/v#{version}/Swarm_#{version}_#{arch}.dmg"
   name "Swarm"
